f0f0d844b78dfc89e5cea6e577a367c81edc45d9
Richard Tillies <rtillies@gmail.com>
2022-03-31 17:28:57 -0400

    feat: TrickOrTreater passes all tests

--- exercise-1/spec/trick_or_treater_spec.rb
+++ exercise-1/spec/trick_or_treater_spec.rb
@@ -11,32 +11,32 @@ RSpec.describe TrickOrTreater do
     expect(trick_or_treater.dressed_up_as).to eq('Cowboy')
   end
 
-  xit 'can have a different costume' do
+  it 'can have a different costume' do
     trick_or_treater = TrickOrTreater.new(Costume.new('Alien'))
 
     expect(trick_or_treater.dressed_up_as).to eq('Alien')
   end
 
-  xit 'has an empty bag by default' do
+  it 'has an empty bag by default' do
     trick_or_treater = TrickOrTreater.new(Costume.new("Alien"))
 
     expect(trick_or_treater.bag.empty?).to be true
   end
 
-  xit 'has an empty bag, so no candies' do
+  it 'has an empty bag, so no candies' do
     trick_or_treater = TrickOrTreater.new(Costume.new("Knight"))
 
     expect(trick_or_treater.has_candy?).to be false
   end
 
-  xit 'can get candies' do
+  it 'can get candies' do
     trick_or_treater = TrickOrTreater.new(Costume.new('Spaceship Mechanic'))
     trick_or_treater.bag.add_candy Candy.new('Gummy bears')
 
     expect(trick_or_treater.has_candy?).to be true
   end
 
-  xit 'it can count candies' do
+  it 'it can count candies' do
     trick_or_treater = TrickOrTreater.new(Costume.new('Spaceship Mechanic'))
 
     expect(trick_or_treater.candy_count).to eq(0)
@@ -46,7 +46,7 @@ RSpec.describe TrickOrTreater do
     expect(trick_or_treater.candy_count).to eq(1)
   end
 
-  xit 'can eat candies' do
+  it 'can eat candies' do
     trick_or_treater = TrickOrTreater.new(Costume.new("Baron"))
     trick_or_treater.bag.add_candy Candy.new("Gummy worms")
     trick_or_treater.bag.add_candy Candy.new("Liquorice")
